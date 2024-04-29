ba21ddc638664b6396fcc4a1ec2fdef3098f1b3d
NadyaPronina <nadya.cats@gmail.com>
2024-04-29 12:34:46 +0300

    new central

--- central/api.py
+++ central/api.py
@@ -1,2 +1,11 @@
 from flask import Flask, jsonify
 
+app = Flask(__name__)
+
+@app.route("/hello",methods=['GET'])
+def hello_connection():
+    message = {"message" : "hi, Albina! It's Nadya fron central"}
+    return jsonify(message)
+
+if __name__ == "__main__":
+    app.run(port=8001)
