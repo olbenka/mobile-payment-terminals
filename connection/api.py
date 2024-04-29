654f9e469db63e63da80317b0f7ececb12bb5699
NadyaPronina <nadya.cats@gmail.com>
2024-04-29 15:22:27 +0300

    attempt with docker

--- connection/api.py
+++ connection/api.py
@@ -1,11 +1,26 @@
 from flask import Flask, jsonify
+from producer import func_producer
 
 app = Flask(__name__)
 
-@app.route("/hello",methods=['GET'])
-def hello_connection():
-    message = {"message" : "hello from connection microservice!"}
+@app.route("/connection",methods=['POST'])
+def central_message():
+    message = {"message" : "It's connection"}
+    func_producer(message)
     return jsonify(message)
 
 if __name__ == "__main__":
     app.run(port=8000)
+
+    
+    
+# как запускать:
+# 1. запустить producer(?)
+# 2. в терминале в папке этой python3 api.py
+# в постмане проверяла, все норм возвращает. чтобы проверить:
+#  1. выбрать пост запрос и адрес http://localhost:8001/central
+#  2. в json написала 
+# {
+#     "key": "value"
+# }, но без этого тоже все работает (проверила)
+# и отправить запрос. возвращает 200 и само сообщение (победа)
