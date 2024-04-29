1c21609bea0e708a96118aa09f91edc32a2f86eb
NadyaPronina <nadya.cats@gmail.com>
2024-04-29 15:44:56 +0300

    upd

--- central/producer.py
+++ central/producer.py
@@ -2,17 +2,14 @@ import json
 import pika
 
 def func_producer(message):
-    connection = pika.BlockingConnection()
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
     channel = connection.channel()
 
     exchange_name = 'central_name'
     routing_key = 'central_queue'
-
-    channel.exchange_declare(exchange=exchange_name, exchange_type='topic', durable=True)
-
-    message_json = json.dumps(message)
     
-    channel.basic_publish(exchange=exchange_name, routing_key=routing_key, body=message_json)
+    channel.exchange_declare(exchange=exchange_name, exchange_type='direct', durable=True)
+    channel.basic_publish(exchange=exchange_name, routing_key=routing_key, body=json.dumps(message))
     
     print("work_central")
     connection.close()
--- connection/producer.py
+++ connection/producer.py
@@ -2,17 +2,14 @@ import json
 import pika
 
 def func_producer(message):
-    connection = pika.BlockingConnection()
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
     channel = connection.channel()
 
     exchange_name = 'connection_name'
     routing_key = 'connection_queue'
-
-    channel.exchange_declare(exchange=exchange_name, exchange_type='topic', durable=True)
-
-    message_json = json.dumps(message)
     
-    channel.basic_publish(exchange=exchange_name, routing_key=routing_key, body=message_json)
+    channel.exchange_declare(exchange=exchange_name, exchange_type='direct', durable=True)
+    channel.basic_publish(exchange=exchange_name, routing_key=routing_key, body=json.dumps(message))
     
     print("work_connection")
     connection.close()
