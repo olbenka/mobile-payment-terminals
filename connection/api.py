16505aa2cd5501e4f7e99f0fe8cbc7e4cf1dfd4d
Albina <albinacneg55@gmail.com>
2024-04-29 12:28:12 +0300

    connection code

--- connection/api.py
+++ connection/api.py
@@ -1,1 +1,11 @@
 from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/hello",methods=['GET'])
+def hello_connection():
+    message = {"message" : "hello from connection microservice!"}
+    return jsonify(message)
+
+if __name__ == "__main__":
+    app.run(port=8000)
